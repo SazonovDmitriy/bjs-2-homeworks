d8fffd35dfd03d79e6835103fb03a2333f91ef4f
SazonovDmitriy <sazonovd21@mail.ru>
2021-06-26 21:40:51 +0300

    Version 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,33 @@
+"use strict"
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+  let arr = [];
+  let D = b**2 - 4 * a * c;
+  let x = 0;
+  let x1 = 0;
+  let x2 = 0;
+  if (D > 0) {
+    return x1 = [(-b + Math.sqrt(D) ) / (2 * a)], x2 = [(-b - Math.sqrt(D) ) / (2 * a)];
+  } else if (D < 0) {
+    return [];
+  } else (D === 0) {
+    return x = [-b / (2 * a)];
+  }
+};
 
+"use strict"
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
-
-  // код для задачи №2 писать здесь
-
+  let percentRate = number(percent);
+  let sumFirst = number(contribution);
+  let sumCredit = number(amount);
+  let time;
+  if (percentRate === Infinity) {
+    `Параметр "процентнтая ставка" содержит неправильное значение ${percent}`
+  } else if (sumFirst === Infinity) {
+    `Параметр "сумма первоначального взноса" содержит неправильное значение ${contribution}`
+  } else (sumCredit === Infinity) {
+    `Параметр "сумма кредита" содержит неправильное значение ${amount}`
+  }
   return totalAmount;
+  // new Date().getMonth()
 }
