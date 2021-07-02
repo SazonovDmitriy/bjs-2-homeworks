a62e41bf8fbdec8d750899e0c201d5a86cdb2f97
SazonovDmitriy <sazonovd21@mail.ru>
2021-07-02 18:39:25 +0300

    V.2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -13,26 +13,33 @@ function getArrayParams(arr) {
     sum += element;
   }
   let avg = (sum / arr.length).toFixed(2);
-  return { min:min, max:max, avg:avg };// начальный код
+  return { min, max, avg };
 }
 
 // Задание 2
-function worker(...arr) {
-  let sum =  Math.max(...arr);
+function worker(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
   return sum;
-};
-console.log(worker([1,2,3],[4,5,6]);
+}
 function makeWork(arrOfArr, func) {
   let max = 0;
   for (let i = 0; i < arrOfArr.length; i++) {
-  let result = worker(arrOfArr[i]);
-  if (sum > max) {
-    max
+  let result = func(arrOfArr[i]);
+  if (result > max) {
+    max = result;
   }
   } return max;
 };
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let diff = 0;
+  for (let i = 0; i < arr.length; i++) {
+    diff += arr[i];
+  }
+  return diff;
 }
+makeWork(arrOfArr, worker2)
--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,10 +1,13 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  let result = ;
+  }
+};
+// compareArrays([8, 9], [6]) // false, разные значения
+// compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]) // false, разные значения
+// compareArrays([9, 2, 4, 8, 2], [9, 2, 4]) // false, разные значения
+// compareArrays([1, 2, 3], [2, 3, 1]) // false, разные индексы, хотя и одинаковые значения
+// compareArrays([8, 1, 2], [8, 1, 2]) // true
 
-  // Ваш код
-
-  return result; // boolean
-}
 
 function advancedFilter(arr) {
   let resultArr;
